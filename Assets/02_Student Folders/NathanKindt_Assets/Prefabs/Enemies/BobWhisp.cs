7eb7445bb80a59f99fa30f6d7a8d9864768129b1
Nathan <N.P.A.Kindt@student.tudelft.nl>
2022-10-20 11:43:00 +0200

    Balanced wands

---
+++ Assets/02_Student Folders/NathanKindt_Assets/Prefabs/Enemies/BobWhisp.cs
@@ -0,0 +1,35 @@
+using UnityEditor;
+using UnityEngine;
+
+public class BobWhisp : MonoBehaviour
+{
+    [Tooltip("Frequency at which the weapon will move around in the screen when the player is not in movement")]
+    public float bobFrequency = 1f;
+    [Tooltip("How fast the weapon bob is applied, the bigger value the fastest")]
+    public float bobAmplitude = 1f;
+    [Tooltip("FlickerLight")]
+    public GameObject flickerLight;
+    [Tooltip("LightFlicker")]
+    public float flickerFrequency = 1f;
+    [Tooltip("LightFlicker")]
+    public float flickerAmplitude = 1f;
+
+    private Behaviour halo;
+    private float rootY;
+
+    private void Awake()
+    {
+        // halo = (Behaviour)flickerLight.GetComponent("Halo");
+        // halo = new SerializedObject(flickerLight.GetComponent<Halo>());
+        rootY = transform.localPosition.y;
+    }
+
+    private void Update()
+    {
+        var pos = transform.localPosition;
+        var bob = Mathf.Sin(Time.time * bobFrequency) * bobAmplitude;
+        transform.localPosition = new Vector3(pos.x, rootY + bob, pos.z);
+        // halo.enabled = false;
+        // print(getC));
+    }
+}
