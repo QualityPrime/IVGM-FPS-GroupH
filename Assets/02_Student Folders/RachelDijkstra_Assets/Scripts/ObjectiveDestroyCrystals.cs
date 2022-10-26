c9a0774bfd5c5e07721d23ab0bbfdc9fd9cddab0
Rachel Dijkstra <racheldijkstra2001@gmail.com>
2022-10-26 21:57:35 +0200

    objective

---
+++ Assets/02_Student Folders/RachelDijkstra_Assets/Scripts/ObjectiveDestroyCrystals.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveDestroyCrystals : MonoBehaviour
+{
+    [Tooltip("Chose whether you need to kill every enemies or only a minimum amount")]
+    public bool mustKillAllEnemies = true;
+    [Tooltip("This is the amount of crystal kills required")]
+    public int killsToCompleteObjective = 3;
+
+    public GameObject crystal1;
+    public GameObject crystal2;
+    public GameObject crystal3;
+    bool destroyed1;
+    bool destroyed2;
+    bool destroyed3;
+    Objective m_Objective;
+    int m_KillTotal;
+
+    void Start()
+    {
+        m_Objective = GetComponent<Objective>();
+        DebugUtility.HandleErrorIfNullGetComponent<Objective, ObjectiveKillEnemies>(m_Objective, this, gameObject);
+
+        killsToCompleteObjective = 3;
+
+        // set a title and description specific for this type of objective, if it hasn't one
+        if (string.IsNullOrEmpty(m_Objective.title))
+            m_Objective.title = "Deactivate enemy shield";
+
+        if (string.IsNullOrEmpty(m_Objective.description))
+            m_Objective.description = GetUpdatedCounterAmount();
+
+        destroyed1 = false;
+        destroyed2 = false;
+        destroyed3 = false;
+
+        m_KillTotal = 0;
+    }
+
+    void Update()
+    {
+        if (m_Objective.isCompleted)
+            return;
+
+        if (!destroyed1 && crystal1 == null)
+        {
+            destroyed1 = true;
+            m_KillTotal++;
+        }
+        if (!destroyed2 && crystal2 == null)
+        {
+            destroyed2 = true;
+            m_KillTotal++;
+        }
+        if (!destroyed3 && crystal3 == null)
+        {
+            destroyed3 = true;
+            m_KillTotal++;
+        }
+
+        if(m_KillTotal == 3)
+        {
+            m_Objective.CompleteObjective(string.Empty, GetUpdatedCounterAmount(), "Objective complete : " + m_Objective.title);
+        }
+        else if (m_KillTotal <= 2)
+        {
+            string notificationText = string.Empty;
+            m_Objective.UpdateObjective(string.Empty, GetUpdatedCounterAmount(), notificationText);
+        }
+
+    }
+
+    string GetUpdatedCounterAmount()
+    {
+        return m_KillTotal + " / " + killsToCompleteObjective;
+    }
+}
