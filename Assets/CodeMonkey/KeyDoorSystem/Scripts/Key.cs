1d98b7601a04166383f4b36484b1d2292057fc08
Nathan <N.P.A.Kindt@student.tudelft.nl>
2022-10-27 17:21:46 +0200

    Summary
    Description

---
+++ Assets/CodeMonkey/KeyDoorSystem/Scripts/Key.cs
@@ -0,0 +1,23 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using UnityEngine;
+
+namespace CodeMonkey.KeyDoorSystem.Scripts {
+
+    [CreateAssetMenu(fileName = "Key", menuName = "Add-Ons/Code Monkey/Key Door System/Create Key", order = 1)]
+    public class Key : ScriptableObject {
+        [Tooltip("Color of the Key")]
+        public Color keyColor;
+    }
+
+}
